b1d089c210c712b901c0b5d98a68ae982d750092
Keith Bugeja <bugeja.keith@gmail.com>
2025-05-26 04:05:23 +0200

    Latest front-end deployment

---
+++ frontend/pkg/web_bg.wasm.d.ts
@@ -0,0 +1,26 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const compile_pixardis_source_with_errors: (a: number, b: number) => any;
+export const compile_pixardis_source: (a: number, b: number) => [number, number, number, number];
+export const __wbg_webvm_free: (a: number, b: number) => void;
+export const webvm_step: (a: number, b: number) => any;
+export const webvm_get_framebuffer: (a: number) => [number, number];
+export const create_vm: (a: number, b: number) => number;
+export const step_vm: (a: number, b: number) => any;
+export const get_vm_framebuffer: (a: number) => [number, number];
+export const get_vm_print_output: (a: number) => any;
+export const clear_vm_print_output: (a: number) => void;
+export const load_vm_program: (a: number, b: number, c: number) => void;
+export const webvm_get_print_output: (a: number) => any;
+export const webvm_load_program: (a: number, b: number, c: number) => void;
+export const webvm_clear_print_output: (a: number) => void;
+export const webvm_new: (a: number, b: number) => number;
+export const __wbindgen_exn_store: (a: number) => void;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_export_2: WebAssembly.Table;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __externref_table_dealloc: (a: number) => void;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_start: () => void;
